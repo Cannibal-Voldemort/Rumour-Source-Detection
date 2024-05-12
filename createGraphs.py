714135c6286f4d6c4ee9b947cffbf8dcbb05e916
Cannibal-Voldemort <canniballuckie17@gmail.com>
2024-05-12 17:09:48 +0530

    Initial Commit

---
+++ createGraphs.py
@@ -0,0 +1,21 @@
+
+import random
+import networkx as nx
+
+def createInstance():
+	for j in range(1, 5):
+		fileHandle = open(path+"/instance"+str(j)+".txt","w")
+        G = nx.karate_club_graph()
+        
+		with open('DiffusionModel/InfectedGraph.txt') as file:
+			array = file.readlines()
+			actualSource = array[0]
+			fileHandle.write(actualSource)
+			for i in range(1,len(array)):
+				src, dst = array[i].split(" ")
+				dst = dst.strip('\n')	
+				wt = str("{0:.2f}".format(random.random()))
+				if(wt == '0.00'):
+					wt = '0.02'
+				fileHandle.write(str(src) + " " + str(dst) + " " + wt + "\n")
+		fileHandle.close()
